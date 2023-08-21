0414b4151fb8a9272bf25809a881c959e0ba4518
Martin Pintos <102275218+MartinPintos677@users.noreply.github.com>
2023-08-21 19:34:11 -0300

    userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,50 +1,76 @@
-const { User, Order } = require("../models");
-
-async function getOrders(req, res) {
-  try {
-    const userId = req.params.id;
-    const user = await User.findByPk(userId, { include: Order });
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    return res.status(200).json({ orders: user.orders });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Internal server error" });
-  }
-}
+const { User } = require("../models");
 
 // Display a listing of the resource.
 async function index(req, res) {
   try {
     const users = await User.findAll();
-    return res.status(200).json(users);
+    res.json(users);
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ error: "Internal server error" });
   }
 }
 
+// Display the specified resource.
 async function show(req, res) {
+  const userId = req.params.id;
   try {
-    const { id } = req.params;
-    const user = await User.findByPk(id);
-
+    const user = await User.findByPk(userId);
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ error: "User not found" });
     }
-
-    return res.status(200).json(user);
+    res.json(user);
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ error: "Internal server error" });
   }
 }
 
+// Store a newly created resource in storage.
+async function store(req, res) {
+  const { firstname, lastname, email, password } = req.body;
+  try {
+    const newUser = await User.create({ firstname, lastname, email, password });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ error: "Bad request" });
+  }
+}
+
+// Update the specified resource in storage.
+async function update(req, res) {
+  const userId = req.params.id;
+  const { firstname, lastname, email, password } = req.body;
+  try {
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    await user.update({ firstname, lastname, email, password });
+    res.json({ message: "User updated successfully" });
+  } catch (error) {
+    res.status(400).json({ error: "Bad request" });
+  }
+}
+
+// Remove the specified resource from storage.
+async function destroy(req, res) {
+  const userId = req.params.id;
+  try {
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    await user.destroy();
+    res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+// Exportar los métodos del controlador.
 module.exports = {
   index,
-  getOrders,
   show,
+  store,
+  update,
+  destroy,
 };
